e43ae0f98360e2544335cad3ee6bea84b900abd8
medicetamol <medicetamol@gmail.com>
2025-08-21 03:08:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/medquiz/",   // 👈 IMPORTANT: must match your GitHub repo name
+  base: "/medquiz/", // repo name
+  build: {
+    outDir: "dist",
+  },
 });
