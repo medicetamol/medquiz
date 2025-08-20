11077289d7966e4cbf1892b7996def99e0962090
medicetamol <medicetamol@gmail.com>
2025-08-21 02:36:07 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
 import Home from "./pages/Home";
@@ -8,6 +9,7 @@ import Quiz from "./pages/Quiz";
 import Review from "./pages/Review";
 import Import from "./pages/Import";
 
+// Define routes
 const router = createBrowserRouter(
   [
     {
@@ -26,9 +28,8 @@ const router = createBrowserRouter(
   }
 );
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-                                            
